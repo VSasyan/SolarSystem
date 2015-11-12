5a8e6f8af9d9e289e5118b96bdebf80ef23b4db2
VSasyan <valentin.sasyan@gmail.com>
2015-11-12 16:57:41 +0100

    version OO

--- src/js/class/Astre.js
+++ src/js/class/Astre.js
@@ -5,83 +5,90 @@ function Astre(properties) {
 	this.quality = properties.quality || 128;
 	// Trajectory :
 	this.size = properties.size;
-	this.a = properties.a;
-	this.e = properties.e;
-	this.I = properties.I;
-	this.L = properties.L;
-	this.omega = properties.omega;
-	this.OMEGA = properties.OMEGA;
-	this.mExtended = properties.mExtended || false;
-	if (this.mExtended) {
-		this.b = properties.b;
-		this.c = properties.c;
-		this.s = properties.s;
-		this.f = properties.f;
+	this.positionFixed = properties.positionFixed || false;
+	if (!this.positionFixed) {
+		this.a = properties.a;
+		this.e = properties.e;
+		this.I = properties.I;
+		this.L = properties.L;
+		this.omega = properties.omega;
+		this.OMEGA = properties.OMEGA;
+		this.mExtended = properties.mExtended || false;
+		if (this.mExtended) {
+			this.b = properties.b;
+			this.c = properties.c;
+			this.s = properties.s;
+			this.f = properties.f;
+		}
 	}
 	// WebGL :
-	this.geometry = new THREE.SphereGeometry( this.size, this.quality, this.quality );
-	this.material = new THREE.MeshBasicMaterial( {color: this.color} );
-	this.element = new THREE.Mesh( this.geometry, this.material );
+	this.geometry = new THREE.SphereGeometry(this.size, this.quality, this.quality);
+	this.material = new THREE.MeshBasicMaterial({color: this.color});
+	this.element = new THREE.Mesh(this.geometry, this.material);
 }
 
 Astre.prototype.getPosition = function(T) { // http://ssd.jpl.nasa.gov/txt/aprx_pos_planets.pdf
-	// Computation : elm = elm0 + T * elm';
-	var a = this.a[0] + T * this.a[1];
-	var e = this.e[0] + T * this.e[1];
-	var e_ = 57.29578 * e;
-	var I = (this.I[0] + T * this.I[1]) / 180 * Math.PI;
-	var L = (this.L[0] + T * this.L[1]) / 180 * Math.PI;
-	var omega = (this.omega[0] + T * this.omega[1]) / 180 * Math.PI;
-	var OMEGA = (this.OMEGA[0] + T * this.OMEGA[1]) / 180 * Math.PI;
-	if (this.mExtended) {
-		var b = this.b[0] + T * this.b[1];
-		var c = this.c[0] + T * this.c[1];
-		var s = this.s[0] + T * this.s[1];
-		var f = this.f[0] + T * this.f[1];
-	}
+	if (!this.positionFixed) {
+		// Computation : elm = elm0 + T * elm';
+		var a = this.a[0] + T * this.a[1];
+		var e = this.e[0] + T * this.e[1];
+		var e_ = 57.29578 * e;
+		var I = (this.I[0] + T * this.I[1]) / 180 * Math.PI;
+		var L = (this.L[0] + T * this.L[1]) / 180 * Math.PI;
+		var omega = (this.omega[0] + T * this.omega[1]) / 180 * Math.PI;
+		var OMEGA = (this.OMEGA[0] + T * this.OMEGA[1]) / 180 * Math.PI;
+		if (this.mExtended) {
+			var b = this.b[0] + T * this.b[1];
+			var c = this.c[0] + T * this.c[1];
+			var s = this.s[0] + T * this.s[1];
+			var f = this.f[0] + T * this.f[1];
+		}
 
-	// Approximation of E :
-	var w = omega - OMEGA;
-	var M = L - omega;
-	if (this.mExtended) {M += b * T * T + c * Math.cos(f * T) + s * Math.sin(f * T);}
-	M = M % 180;
-	var En = M + e_ * Math.sin(M);
-	var deltaE = 1;
-	var tol = 1 / 1000;
-	var i = 0;
-	while (Math.abs(deltaE) > tol && i < 1000000) {
-		var deltaM = M - (En - e_ * Math.sin(En));
-		deltaE = deltaM / (1 - e * Math.cos(En));
-		En += deltaE;
-		i++;
-	}
-	var E = En;
-	//console.log(i);
+		// Approximation of E :
+		var w = omega - OMEGA;
+		var M = L - omega;
+		if (this.mExtended) {M += b * T * T + c * Math.cos(f * T) + s * Math.sin(f * T);}
+		M = M % 180;
+		var En = M + e_ * Math.sin(M);
+		var deltaE = 1;
+		var tol = 1 / 1000;
+		//var i = 0;
+		while (Math.abs(deltaE) > tol /*&& i < 1000000*/) {
+			var deltaM = M - (En - e_ * Math.sin(En));
+			deltaE = deltaM / (1 - e * Math.cos(En));
+			En += deltaE;
+			//i++;
+		}
+		var E = En;
+		//console.log(i);
 
-	// Speeds :
-	var x_ = a * (Math.cos(E) - e);
-	var y_ = a * Math.sqrt(1 - e * e) * Math.sin(E);
-	var z_ = 0;
-	
-	// Positions :	
-	var cos_w = Math.cos(w);
-	var sin_w = Math.sin(w);
-	var cos_I = Math.cos(I);
-	var sin_I = Math.sin(I);
-	var cos_OMEGA = Math.cos(OMEGA);
-	var sin_OMEGA = Math.sin(OMEGA);
+		// Speeds :
+		var x_ = a * (Math.cos(E) - e);
+		var y_ = a * Math.sqrt(1 - e * e) * Math.sin(E);
+		var z_ = 0;
+		
+		// Positions :	
+		var cos_w = Math.cos(w);
+		var sin_w = Math.sin(w);
+		var cos_I = Math.cos(I);
+		var sin_I = Math.sin(I);
+		var cos_OMEGA = Math.cos(OMEGA);
+		var sin_OMEGA = Math.sin(OMEGA);
 
-	var position = {
-		x : (cos_w * cos_OMEGA - sin_w * sin_OMEGA * cos_I) * x_ + (-sin_w * cos_OMEGA - cos_w * sin_OMEGA * cos_I) * y_,
-		y : (cos_w * sin_OMEGA + sin_w * cos_OMEGA * cos_I) * x_ + (-sin_w * sin_OMEGA + cos_w * cos_OMEGA * cos_I) * y_,
-		z : (sin_w * cos_I) * x_ + (cos_w * sin_I) * y_
-	};
-	return position;
+		var position = {
+			x : (cos_w * cos_OMEGA - sin_w * sin_OMEGA * cos_I) * x_ + (-sin_w * cos_OMEGA - cos_w * sin_OMEGA * cos_I) * y_,
+			y : (cos_w * sin_OMEGA + sin_w * cos_OMEGA * cos_I) * x_ + (-sin_w * sin_OMEGA + cos_w * cos_OMEGA * cos_I) * y_,
+			z : (sin_w * cos_I) * x_ + (cos_w * sin_I) * y_
+		};
+		return position;
+	} else {return false;}
 }
 
 Astre.prototype.setPosition = function(T) {
-	var position = this.getPosition(T);
-	this.element.position.x = position.x;
-	this.element.position.y = position.y;
-	this.element.position.z = position.z;
+	if (!this.positionFixed) {
+		var position = this.getPosition(T);
+		this.element.position.x = position.x;
+		this.element.position.y = position.y;
+		this.element.position.z = position.z;
+	}
 }
--- src/js/script.js
+++ src/js/script.js
@@ -1,87 +1,59 @@
 var T = 0;
 var t = 0;
-setInterval(function() {t += 1;}, 50);
+var astres = [];
+var camera;
+var camControls;
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+function init() {
+	setInterval(function() {t += 1;}, 50);
+	var clock = new THREE.Clock();
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+	var scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+	var renderer = new THREE.WebGLRenderer();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	document.body.appendChild(renderer.domElement);
 
-Soleil = new Astre({
-	size : 0.1,
-	color : 0xffff00,
-	name : 'Soleil'
-});
-Venus = new Astre({
-	size : 0.02,
-	color : 0xffff55,
-	name : 'Vénus',
-	a : [0.72333566, 0.00000390],
-	e : [0.00677672, -0.00004107],
-	I : [3.39467605, -0.00078890],
-	L : [181.97909950, 58517.81538729],
-	omega : [131.60246718, 0.00268329],
-	OMEGA : [76.67984255, -0.27769418],
-	mExtended : false
-});
-Terre = new Astre({
-	size : 0.05,
-	color : 0x0000ff,
-	name : 'Terre',
-	a : [1.00000261, 0.00000562],
-	e : [0.01671123, -0.00004392],
-	I : [-0.00001531, -0.01294668],
-	L : [100.46457166, 35999.37244981],
-	omega : [102.93768193, 0.32327364],
-	OMEGA : [0.0, 0.0],
-	mExtended : false
-});
-Mars = new Astre({
-	size : 0.03,
-	color : 0xff0000,
-	name : 'Mars',
-	a : [1.52371034, 0.00001847],
-	e : [0.09339410, 0.00007882],
-	I : [1.84969142, -0.00813131],
-	L : [-4.55343205, 19140.30268499],
-	omega : [-23.94362959, 0.44441088],
-	OMEGA : [49.55953891, -0.29257343],
-	mExtended : false
-});
-Jupiter = new Astre({
-	size : 0.07,
-	color : 0xffffff,
-	name : 'Jupiter',
-	a : [5.20288700, 0.04838624],
-	e : [0.09339410, 0.00007882],
-	I : [1.30439695, -0.00183714],
-	L : [34.39644051, 3034.74612775],
-	omega : [14.72847983, 0.21252668],
-	OMEGA : [100.47390909, 0.20469106],
-	mExtended : false
-});
+	$.ajax({
+		url : 'astres.json.php',
+		type : 'GET',
+		success : function(data) {
+			dataAstres = JSON.parse(data);
+			$.each(dataAstres, function(i,d) {
+				var astre = new Astre(d);
+				scene.add(astre.element);
+				astres.push(astre);
+			});
+		}
+	})
 
-scene.add(Soleil.element);
-scene.add(Venus.element);
-scene.add(Terre.element);
-scene.add(Mars.element);
-scene.add(Jupiter.element);
-camera.position.z = 3;
+	/*camControls = new THREE.FirstPersonControls(camera);
+	camControls.lookSpeed = 0;
+	camControls.movementSpeed = 10;
+	camControls.lookVertical = true;
+*/
+	camera.position.x = 0;
+	camera.position.y = 0;
+	camera.position.z = 3;
+	camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-var render = function () {
-	requestAnimationFrame(render);
+	var render = function () {
+		requestAnimationFrame(render);
 
-	if (T != t / 100000) {
-		T = t / 100000;
-		Venus.setPosition(T);
-		Terre.setPosition(T);
-		//Mars.setPosition(T);
-		//Jupiter.setPosition(T);
-	}
+		if (T != t / 100000) {
+			T = t / 100000;
+			$.each(astres, function(i, astre) {
+				astre.setPosition(T);
+			});
+		}
 
-	renderer.render(scene, camera);
-};
-render();
+		var delta = clock.getDelta();
+		//camControls.update(delta);
+		renderer.render(scene, camera);
+	};
+	render();
+
+}
+
+window.onload = init;
